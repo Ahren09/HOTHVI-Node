25246bffd9a22c98fd419e2499f4a649a1648fe0
punteek <prateeksingh1@hotmail.com>
2019-02-17 02:35:31 -0800

    initial files

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+const { saveTweet, updateLikes } = require('./database.js');
+
+class Tweet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numLike: 0
+    };
+
+    this.buttonOnClick = () => { this.incrementLike(); };
+  }
+
+  incrementLike() {
+    const previousLike = this.state.numLike;
+    const newState = {
+      numLike: previousLike + 1
+    };
+
+    // search up tweet by content, then update the like count
+    // console.log('trying to update tweets');
+    // console.log(this.props.tweet);
+    // console.log(this.state.numLike);
+    updateLikes(this.props.tweet, this.state.numLike);
+
+    this.setState(newState);
+  }
+
+  render() {
+    const numLike = this.state.numLike;
+    return (
+      <div>
+        <h2>{this.props.tweet}</h2>
+        <button onClick={this.buttonOnClick}>
+          <span role="img" aria-label="Love">❤️</span> {numLike}
+        </button>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tweets: [],
+      currTweet: ''
+    };
+    this.tweetIndex = 0;
+    this.inputOnChange = (e) => { this.updateCurrTweet(e); };
+    this.buttonOnClick = () => { this.addTweet(); };
+  }
+
+  updateCurrTweet(event) {
+    console.log(event);
+    const newState = {
+      currTweet: event.target.value
+    };
+
+    this.setState(newState);
+  }
+
+  addTweet() {
+    if (this.state.currTweet === '') {
+      alert('Input something first');
+      return;
+    }
+    const currTweetObj = {
+      index: this.tweetIndex,
+      content: this.state.currTweet
+    };
+
+    // push new tweet to database
+    saveTweet(currTweetObj.content, 0);
+
+    this.tweetIndex += 1;
+
+    const prevTweets = this.state.tweets;
+    const newTweets = [currTweetObj, ...prevTweets];
+    const newState = {
+      tweets: newTweets,
+      currTweet: ''
+    };
+
+    this.setState(newState);
+  }
+  render() {
+    const tweets = this.state.tweets;
+    const lists = tweets.map((tweetObj) => <Tweet tweet={tweetObj.content} key={tweetObj.index} />);
+    return (
+      <div>
+        <input value={this.state.currTweet} onChange={this.inputOnChange}/>
+        <button onClick={this.buttonOnClick}>tweet</button>
+        {lists}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/database.js
@@ -0,0 +1,36 @@
+const firebase = require("firebase/app");
+require('@firebase/database');
+
+// Firebase Configuration
+let config = {
+  apiKey: "AIzaSyA1T4TEKc4XGW9criEyrk1GOmgyysohDTI",
+  authDomain: "hothvi-node.firebaseapp.com",
+  databaseURL: "https://hothvi-node.firebaseio.com",
+  projectId: "hothvi-node",
+  storageBucket: "hothvi-node.appspot.com",
+  messagingSenderId: "888477214159"
+};
+firebase.initializeApp(config);
+let database = firebase.database();
+
+function saveTweet(curTweet, likes){
+  database.ref('tweets').push({
+    tweetText: curTweet,
+    numLikes: likes
+  })
+}
+
+function updateLikes(tweetContent, newLikeCount){
+  console.log('going into update!!!');
+  console.log("tweetContent: " + tweetContent);
+  console.log("newLikeCount: " + newLikeCount);
+  let query = database.ref('tweets').orderByChild('tweetText').equalTo(tweetContent);
+  query.once("child_added", function(snapshot){
+    snapshot.ref.update({numLikes: newLikeCount});
+  });
+}
+
+module.exports = {
+  saveTweet,
+  updateLikes
+};
